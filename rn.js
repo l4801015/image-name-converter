748b5c668f4ce29780b18995f4f5dac194cf0291
Alex <l4801015@gmail.com>
2021-05-05 21:56:15 +0300

    Change name pattern for search

--- rn.js
+++ rn.js
@@ -12,11 +12,10 @@ const files = readdirSync(imageDirPath);
 
 for (i = 0; i < files.length; i++) {
 
-  const before = files[i]
-
+  const before = "Petrov A.D.jpeg"
   const temp = before.split(".")
-
-  const after = temp[1] + "." + temp[0] + ".png"
+  const tempName = temp[0].split(" ")
+  const after = temp[1] + "." + tempName[0] + "." + temp[2]
 
   rename(
 
